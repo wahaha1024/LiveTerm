b603418df50226253d0b06ae4723fb2cb14112d2
wahaha1024 <61113260+wahaha1024@users.noreply.github.com>
2025-03-18 22:37:15 +0800

    Update commands.ts

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -141,28 +141,21 @@ export const sudo = async (args?: string[]): Promise<string> => {
 // Banner
 export const banner = (args?: string[]): string => {
   return `
-  < 多读书，多看报，少吃零食多睡觉 >
- --------------------------------
-            \
-             \
-              \
-      /\  /\  /\  /\  /\  /\
-     /  \/  \/  \/  \/  \/  \
-    |                        |
-    |                        |               GO!GO!GO! 
-    | __----__      __----__ |              -----------   
-    |/      * \    /      * \|               <u><a href="https://116.205.124.62:42759/4f8c6eab" target="_blank">控制面板</a></u>
-    |          |  |          |             
-  --|\        /    \        /|--           
- /  | --____--      --____-- |  \           爵 <u><a href="http://116.205.124.62:5244/" target="_blank">我的仓库</a></u>
-| (-|                        |-) |          -----------
- \  |.                      .|  /        
-  --/                        \--        
-   /                          \
-    `--______________________--'
-      \_|__|__|__|__|__|_|_/
-        `---__      __---'
-              )(
+           ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▄                  sumfetch
+        ▄▓▓▀ ▄▓▓▀▓▓▓▀▓▓▄ ▀▀▓▓▄              -----------
+      ▓▓▀  ▄▓▀   ▐▓▓  ▀▓▓    ▓▓▄             ABOUT
+    ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓            ${config.name}
+   ▓▓     ▓▓▓    ▐▓▓    ▐▓▓     ▓▓           <u><a href="${config.resume_url}" target="_blank">resume</a></u>
+▐▓▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▓       爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
+▐▓                                 ▐▓       -----------
+▐▓        > L I V E T E R M        ▐▓        CONTACT 
+▐▓                                 ▐▓        <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
+▐▓▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▓        <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
+   ▓▓      ▐▓▓    ▓▓    ▐▓▓     ▓▓           <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
+    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓           -----------
+      ▓▓▓   ▐▓▓   ▓▓   ▓▓▓   ▓▓▀             DONATE 
+        ▀▓▓▄▄ ▀▓▓▄▓▓▄▓▓▓▄▄▓▓▀                <u><a href="${config.donate_urls.paypal}" target="_blank">${config.donate_urls.paypal}</a></u>
+            ▀▓▓▓▓▓▓▓▓▓▓▓▀▀                   <u><a href="${config.donate_urls.patreon}" target="_blank">${config.donate_urls.patreon}</a></u>
 
 `;
 };
